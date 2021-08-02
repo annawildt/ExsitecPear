fe832ce57b1738bc1019c236c2f802150904916a
Anna Wildt <anna.wildt@hotmail.com>
2021-08-02 17:33:48 +0200

    All methods created

--- src/com/pear/Order.java
+++ src/com/pear/Order.java
@@ -1,22 +1,41 @@
 package com.pear;
 
-import java.sql.Timestamp;
+import java.sql.Date;
 
 public class Order {
     private int orderID;
     private String userID;
-    private java.sql.Timestamp date;
+    private java.sql.Date date;
     private Product product;
     private int amount;
     private StorageUnit storageUnit;
+    private String status;
 
-    public Order(int orderID, String userID, Timestamp date, Product product, int amount, StorageUnit storageUnit) {
+    public Order(String userID, java.sql.Date date, Product product, int amount, StorageUnit storageUnit) {
+        this.userID = userID;
+        this.date = date;
+        this.product = product;
+        this.amount = amount;
+        this.storageUnit = storageUnit;
+    }
+
+    public Order(int orderID, String userID, Date date, Product product, int amount, StorageUnit storageUnit) {
         this.orderID = orderID;
         this.userID = userID;
         this.date = date;
         this.product = product;
         this.amount = amount;
         this.storageUnit = storageUnit;
+    }
+
+    public Order(int orderID, String userID, Date date, Product product, int amount, StorageUnit storageUnit, String status) {
+        this.orderID = orderID;
+        this.userID = userID;
+        this.date = date;
+        this.product = product;
+        this.amount = amount;
+        this.storageUnit = storageUnit;
+        this.status = status;
     }
 
     public int getOrderID() {
@@ -27,7 +46,7 @@ public class Order {
         return userID;
     }
 
-    public Timestamp getDate() {
+    public java.sql.Date getDate() {
         return date;
     }
 
@@ -41,5 +60,9 @@ public class Order {
 
     public StorageUnit getStorageUnit() {
         return storageUnit;
+    }
+
+    public String getStatus() {
+        return status;
     }
 }
