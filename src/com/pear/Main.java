76e766c398e9203a6ffb1f00b788aad1c79ee4d5
Anna Wildt <anna.wildt@hotmail.com>
2021-08-04 09:17:49 +0200

    Remove waste

--- src/com/pear/Main.java
+++ src/com/pear/Main.java
@@ -1,6 +1,5 @@
 package com.pear;
 
-import java.sql.Date;
 import java.sql.SQLException;
 
 public class Main {
@@ -12,15 +11,5 @@ public class Main {
         do {
             runApp = menu.printMenu();
         } while (runApp);
-
-//        DatabaseConnection db = new DatabaseConnection();
-//        db.orderHandlingUpdates(new Order(5,
-//                "Placeholder",
-//                Date.valueOf("2021-07-31"),
-//                (new Product("P002", "jPlatta", 5700)),
-//                50,
-//                (new StorageUnit(2, "Norrköping"))));
-//
-//        db.closeConnection();
     }
 }
